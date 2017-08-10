04d99bf25ddc7f0b7f61cd3034538fa146cb5b38
resilientcoders6 <arnold.pires92@gmail.com>
2017-08-10 18:10:07 -0400

    more stuff

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ class App extends React.Component {
     return (
       <Scene>
         <a-assets>
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
+          <img id="groundTexture" src="https://ucarecdn.com/0e2acdd8-a760-4801-97ca-7eeb88c6dd62/"/>
           <img id="skyTexture" src="https://ucarecdn.com/e21a6bc9-cc22-4202-8d0e-f91a4e8e326c/"/>
         </a-assets>
 
@@ -33,6 +33,7 @@ class App extends React.Component {
         <Entity primitive="a-sky" height="2348" radius="30" src="#skyTexture" theta-length="90" width="2348"/>
         <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
         <Entity text={{value: 'Hello! This is A-Frame ReactVR!!!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+        <Entity text={{value: 'Random Text Random Text', align: 'center'}} position={{x: 0, y: 2, z: -3}}/>
 
         <Entity id="box"
           geometry={{primitive: 'box'}}
@@ -41,7 +42,7 @@ class App extends React.Component {
           animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
           position={{x: 0, y: 1, z: -3}}
           events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 500, loop: true, to: '2 2 2'}}
                   geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
                   material={{color: '#24CAFF'}}/>
         </Entity>
@@ -53,7 +54,7 @@ class App extends React.Component {
           animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
           position={{x: 0, y: 1, z: -7}}
           events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 600, loop: true, to: '2 2 2'}}
                   geometry={{primitive: 'sphere', depth: 0.2, height: 0.2, width: 0.2}}
                   material={{color: '#24CAFF'}}/>
         </Entity>
@@ -65,7 +66,7 @@ class App extends React.Component {
           animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
           position={{x: 0, y: 9, z: -4}}
           events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 10000, loop: true, to: '2 2 2'}}
                   geometry={{primitive: 'sphere', depth: 0.2, height: 0.2, width: 0.2}}
                   material={{color: 'yellow'}}/>
         </Entity>
@@ -82,9 +83,17 @@ class App extends React.Component {
                   material={{color: '#e20505'}}/>
         </Entity>
 
+        <a-ocean position="0 0 0"
+        geometry="primitive: plane; width: 100; height: 200;" rotation="-90 0 0"
+        material="src: #grid; repeat: 5 10; ;metalness:0.6; transparent: true; opacity: 0.7; sphericalEnvMap: #sky;">
+    </a-ocean>
+        <a-entity geometry="primitive: plane; height: 1.00; width: 2.10" scale="10 15 10" position="7.35 7.41 1.45" rotation="0 -80 0"  material="shader: flat; transparent: true;
+        src: url(https://ucarecdn.com/075ac85d-290a-42a2-814f-1738a69be028/)"></a-entity>
+
         <Entity primitive="a-camera">
           <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
         </Entity>
+
       </Scene>
     );
   }
